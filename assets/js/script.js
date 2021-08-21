5e2a864a216758666bb73d0376542879992b7195
Esper06 <matt.walford98@gmail.com>
2021-08-21 19:11:12 +0100

    finished prompts for the main js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const InputPrompt = require('inquirer/lib/prompts/input');
 const Manager = require('./constructors/manager');
+const Employee = require('./constructors/base');
 
 //an array we will be using later
 const teamArray = [];
@@ -50,3 +51,72 @@ const addManager = () => {
         console.log(manager);
     })
 };
+
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type:'list',
+            name:'role',
+            message:"Please select the type of employee you would like to add",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type:'input',
+            name:'name',
+            message:"What is the employee's name?"
+        },
+        {
+            type:'input',
+            name:'id',
+            message:"Please enter the employee's ID"
+        },
+        {
+            type: 'input',
+            name:'email',
+            message:"Please enter the employee's email"
+        },
+        {
+            type:'input',
+            name:'github',
+            message:"Please enter the employee's github username",
+            when: (input) => input.role === "Engineer" //This makes it so that this question is only asked if the employee has the role engineer 
+        },
+        {
+            type: 'input',
+            name:'school',
+            message:"Please enter the intern's school",
+            when: (input) => input.role === "Intern" //This makes it so that this question is only asked if the employee has the role intern
+        },
+        {
+            type:'confirm',
+            name:'confirmAddEmployee',
+            message:"Would you like to add more employees?",
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        //this takes the data from the prompt and assigns it to a variable called employeeData
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+        let employee; //we make this variable for future use
+        
+        if (role === "Engineer") {  //if the role is engineer then they get the engineer data assigned to it
+            employee = new Engineer (name, id, email, github)
+
+            console.log(employee);
+
+        } else if (role === "Intern") { //likewise if they're an intern then they get the intern data assigned to it
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee)
+        }
+
+        teamArray.push(employee); //we then push the data to the teamArray we made earlier
+
+        if (confirmAddEmployee) { //if they answered yes to adding another employee then the prompt function as ran again
+            return addEmployee(teamArray);
+        } else {
+            return teamArray; //if not then it returns the array with all the data in
+        }
+    })
+};
+
